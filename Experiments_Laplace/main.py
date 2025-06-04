92214228b32765107a28eec8b99e10ee32e8f868
LDP-Based-Analysis <lijiayu@iie.ac.cn>
2025-06-04 19:44:54 +0800

    V2.0

--- Experiments_Laplace/main.py
+++ Experiments_Laplace/main.py
@@ -15,26 +15,26 @@ str4="dataset/data_synthetic.txt" #zipf distribution, parameter=1.5, num=1_000_0
 
 data_list=data_read.data_read(str4)
 #
-# # Experiments related to, various e_1
-# print("Experiment 1 begin: various e_1")
-# epsilon_sum_list=[0.1,0.3,0.5,0.7,0.9,1.1,1.3]# 固定A和B的比例为1：3，调整总隐私预算
-# epsilon_tuple=[[epsilon_sum*2/3,epsilon_sum/3] for epsilon_sum in epsilon_sum_list]
-#
-# for epsilon_list in epsilon_tuple:
-#     data = test.x_2_star_test(data_list,epsilon_list,test_time = 10)
-#     print("e_1:",sum(epsilon_list),"\t","\t".join(map(str, data)))
-#
-#
-# # Experiments related to, various e_2/e_1
-# print("Experiment 2 begin: various e_2/e_1")
-# alpha=0.5
-# alpha_list=[0.5,0.7,0.9]
-# epsilon_sum=1
-# epsilon_tuple=[[epsilon_sum*i,epsilon_sum*(1-i)] for i in alpha_list]###固定总隐私预算，调整A和B之间的比例
-#
-# for epsilon_list in epsilon_tuple:
-#     data = test.x_2_star_test(data_list,epsilon_list,test_time = 10)
-#     print("e_2/e_2:",epsilon_list[0]/sum(epsilon_list),"\t","\t".join(map(str, data)))
+# Experiments related to, various e_1
+print("Experiment 1 begin: various e_1")
+epsilon_sum_list=[0.1,0.3,0.5,0.7,0.9,1.1,1.3]# 固定A和B的比例为1：3，调整总隐私预算
+epsilon_tuple=[[epsilon_sum*2/3,epsilon_sum/3] for epsilon_sum in epsilon_sum_list]
+
+for epsilon_list in epsilon_tuple:
+    data = test.x_2_star_test(data_list,epsilon_list,test_time = 10)
+    print("e_1:",sum(epsilon_list),"\t","\t".join(map(str, data)))
+
+
+# Experiments related to, various e_2/e_1
+print("Experiment 2 begin: various e_2/e_1")
+alpha=0.5
+alpha_list=[0.5,0.7,0.9]
+epsilon_sum=1
+epsilon_tuple=[[epsilon_sum*i,epsilon_sum*(1-i)] for i in alpha_list]###固定总隐私预算，调整A和B之间的比例
+
+for epsilon_list in epsilon_tuple:
+    data = test.x_2_star_test(data_list,epsilon_list,test_time = 10)
+    print("e_2/e_2:",epsilon_list[0]/sum(epsilon_list),"\t","\t".join(map(str, data)))
 
 # Experiments related to, various the number of servers k
 print("Experiment 3 begin: various server count k")
--- Experiments_SR/main.py
+++ Experiments_SR/main.py
@@ -17,25 +17,25 @@ column = 3
 
 true_list=list(data[column])
 
-# # Experiments related to, various e_1
-# print("Experiment 1 begin: various e_1")
-# epsilon_sum_list=[0.1,0.3,0.5,0.7,0.9,1.1,1.3]
-# epsilon_tuple=[[epsilon_sum*2/3,epsilon_sum/3] for epsilon_sum in epsilon_sum_list]
-#
-# for epsilon_list in epsilon_tuple:
-#     test.SR_test(true_list,epsilon_list,100)
-# #the output correspond to naive select larger privacy budget, baseline, reuse-based perturbation respectively
-#
-#
-# # Experiments related to, various e_2/e_1
-# print("Experiment 2 begin: various e_2/e_1")
-# alpha_list=[0.5,0.7,0.9]
-# epsilon_sum=1
-# epsilon_tuple=[[epsilon_sum*i,epsilon_sum*(1-i)] for i in alpha_list]
-#
-# for epsilon_list in epsilon_tuple:
-#     test.SR_test(true_list,epsilon_list,100)
-# #the output correspond to naive select larger privacy budget, baseline, reuse-based perturbation respectively
+# Experiments related to, various e_1
+print("Experiment 1 begin: various e_1")
+epsilon_sum_list=[0.1,0.3,0.5,0.7,0.9,1.1,1.3]
+epsilon_tuple=[[epsilon_sum*2/3,epsilon_sum/3] for epsilon_sum in epsilon_sum_list]
+
+for epsilon_list in epsilon_tuple:
+    test.SR_test(true_list,epsilon_list,100)
+#the output correspond to naive select larger privacy budget, baseline, reuse-based perturbation respectively
+
+
+# Experiments related to, various e_2/e_1
+print("Experiment 2 begin: various e_2/e_1")
+alpha_list=[0.5,0.7,0.9]
+epsilon_sum=1
+epsilon_tuple=[[epsilon_sum*i,epsilon_sum*(1-i)] for i in alpha_list]
+
+for epsilon_list in epsilon_tuple:
+    test.SR_test(true_list,epsilon_list,100)
+#the output correspond to naive select larger privacy budget, baseline, reuse-based perturbation respectively
 
 
 
